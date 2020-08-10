c53c2e3fbd59b751435120e8eae712befeca3822
Tazwar <tazwar9t6@gmail.com>
2020-08-10 23:13:15 +0600

    done

---
+++ main.js
@@ -0,0 +1,46 @@
+const api = {
+    key: "301d46c3491b6b04520d58c8e4123180",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+const searchBox = document.querySelector(".search-box");
+searchBox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode === 13) {
+        getResults(searchBox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(displayResults);
+}
+
+function displayResults(weather) {
+    let city = document.querySelector(".location .city");
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+    let now = new Date();
+    let date = document.querySelector(".location .date");
+    date.innerText = dateBuilder(now);
+    let temp = document.querySelector(".current .temp");
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span> `;
+    let weatherOfTheCity = document.querySelector(".current .weather");
+    weatherOfTheCity.innerText = `${weather.weather[0].main}`;
+    let hilow = document.querySelector(".current .hi-low");
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+}
+
+function dateBuilder(d) {
+    let months = ["January", "February", "March", "April", "May",
+        "June", "July", "August", "September", "October",
+        "November", "December"
+    ];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let day = days[d.getDay()];
+    let month = months[d.getMonth()];
+    let date = d.getDate();
+    let year = d.getFullYear();
+    return `${day} ${date} ${month} ${year}`;
+}
